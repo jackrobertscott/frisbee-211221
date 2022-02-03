573ee5a5403121ac9aae4f92da13d8608b2c715c
jackrobertscott <jack.rob.scott@gmail.com>
2022-02-03 11:43:16 +0800

    refactored graph and connected to game points

--- packages/browser/src/app/Dashboard/DashboardLadder.ts
+++ packages/browser/src/app/Dashboard/DashboardLadder.ts
@@ -1,20 +1,13 @@
 import {css} from '@emotion/css'
 import dayjs from 'dayjs'
-import {
-  createElement as $,
-  FC,
-  Fragment,
-  useEffect,
-  useRef,
-  useState,
-} from 'react'
+import {createElement as $, FC, Fragment, useEffect, useState} from 'react'
 import {$FixtureListOfSeason, $FixtureUpdate} from '../../endpoints/Fixture'
 import {$TeamListOfSeason} from '../../endpoints/Team'
 import {TFixture} from '../../schemas/ioFixture'
 import {TTeam} from '../../schemas/ioTeam'
 import {theme} from '../../theme'
 import {addkeys} from '../../utils/addkeys'
-import {tallyChart, TTallyChart} from '../../utils/tallyChart'
+import {tallyChart} from '../../utils/tallyChart'
 import {useAuth} from '../Auth/useAuth'
 import {Form} from '../Form/Form'
 import {FormBadge} from '../Form/FormBadge'
@@ -26,11 +19,13 @@ import {useEndpoint} from '../useEndpoint'
 import {FormColumn} from '../Form/FormColumn'
 import {useMedia} from '../Media/useMedia'
 import {initials} from '../../utils/initials'
+import {Graph} from '../Graph'
 /**
  *
  */
 export const DashboardLadder: FC = () => {
   const auth = useAuth()
+  const media = useMedia()
   const $fixtureUpdate = useEndpoint($FixtureUpdate)
   const $teamList = useEndpoint($TeamListOfSeason)
   const $fixtureList = useEndpoint($FixtureListOfSeason)
@@ -51,10 +46,6 @@ export const DashboardLadder: FC = () => {
       $(Form, {
         background: theme.bgMinor,
         children: addkeys([
-          $(_DashboardLadderGraph, {
-            teams,
-            tally,
-          }),
           $(Table, {
             head: {
               name: {label: 'Name', grow: 5},
@@ -128,6 +119,32 @@ export const DashboardLadder: FC = () => {
                           })
                         }),
                       }),
+              }),
+          }),
+          $(Fragment, {
+            children:
+              fixtures !== undefined &&
+              $(Graph, {
+                height:
+                  media.width > theme.fib[13] ? theme.fib[12] : theme.fib[11],
+                title: 'Game Points Scored',
+                yLabel: 'Occurrences',
+                xLabel: 'Points Scored',
+                line: (() => {
+                  const all = fixtures
+                    .map((i) => {
+                      return i.games
+                        .map((x) => [x.team1Score, x.team2Score])
+                        .flat()
+                    })
+                    .flat()
+                    .filter((i) => typeof i === 'number') as number[]
+                  const max = Math.max(...all)
+                  const data = new Array(max + 1).fill(0)
+                  for (let i = 0; i < data.length; i++)
+                    data[i] = all.filter((x) => x === i).length
+                  return data
+                })(),
               }),
           }),
         ]),
@@ -252,113 +269,5 @@ const _LadderFixture: FC<{
           ]),
         }),
     ]),
-  })
-}
-/**
- *
- */
-const _DashboardLadderGraph: FC<{
-  teams: TTeam[]
-  tally: Record<string, TTallyChart | undefined>
-}> = () => {
-  const media = useMedia()
-  const bgRef = useRef<HTMLCanvasElement>()
-  const cvsRef = useRef<HTMLCanvasElement>()
-  useEffect(() => {
-    if (!cvsRef.current || !bgRef.current) return
-    const cvs = cvsRef.current
-    const height = (cvs.height = theme.fib[12])
-    const width = (cvs.width = bgRef.current.clientWidth)
-    const ctx = cvs.getContext('2d')!
-    const axis = 50
-    const yCount = 15
-    const yLength = height - axis
-    const yIncrement = yLength / yCount
-    const xCount = 30
-    const xLength = width - axis
-    const xIncrement = xLength / xCount
-    ctx.lineWidth = 2
-    ctx.strokeStyle = 'rgba(0, 0, 0, 1)'
-    ctx.font = '14px IBM Plex Mono'
-    {
-      // y axis
-      const x = axis
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'
-      ctx.beginPath()
-      ctx.rect(0, 0, axis, height - axis)
-      ctx.fill()
-      ctx.beginPath()
-      ctx.moveTo(x, 0)
-      ctx.lineTo(x, height - axis)
-      ctx.textAlign = 'right'
-      ctx.fillStyle = 'rgba(0, 0, 0, 1)'
-      for (let i = 0; i < yCount; i++) {
-        const y = height - axis - Math.round(i * yIncrement)
-        ctx.moveTo(x, y)
-        ctx.lineTo(x - 5, y)
-        ctx.fillText(i.toString(), x - 10, y + 5)
-      }
-      ctx.stroke()
-      ctx.beginPath()
-      ctx.save()
-      ctx.translate(20, yLength / 2)
-      ctx.rotate(-Math.PI / 2)
-      ctx.textAlign = 'center'
-      ctx.fillText('Y Axis', 0, 0)
-      ctx.restore()
-    }
-    {
-      // x axis
-      const y = height - axis
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'
-      ctx.beginPath()
-      ctx.rect(axis, height - axis, width - axis, axis)
-      ctx.fill()
-      ctx.beginPath()
-      ctx.moveTo(axis, y)
-      ctx.lineTo(width, y)
-      ctx.textAlign = 'center'
-      ctx.fillStyle = 'rgba(0, 0, 0, 1)'
-      for (let i = 0; i < xCount; i++) {
-        const x = axis + Math.round(i * xIncrement)
-        ctx.moveTo(x, y)
-        ctx.lineTo(x, y + 5)
-        ctx.fillText(i.toString(), x, y + 20)
-      }
-      ctx.stroke()
-      ctx.beginPath()
-      ctx.textAlign = 'center'
-      ctx.fillText('X Axis', axis + xLength / 2, height - 10)
-    }
-    {
-      const points = new Array(30)
-        .fill(0)
-        .map((_, i) => Math.floor(Math.random() * 5) + 5)
-      ctx.beginPath()
-      ctx.textAlign = 'center'
-      ctx.fillStyle = 'rgba(255, 255, 255, 1)'
-      for (let i = 0; i < points.length; i++) {
-        const x = axis + Math.round(i * xIncrement)
-        const y = height - axis - Math.round(points[i] * yIncrement)
-        if (i === 0) ctx.moveTo(x, y)
-        else ctx.lineTo(x, y)
-        ctx.fillText(points[i].toString(), x, y + 20)
-      }
-      ctx.stroke()
-    }
-  }, [media.width])
-  return $('div', {
-    ref: bgRef,
-    className: css({
-      display: 'flex',
-      background: theme.bg.string(),
-      border: theme.border(),
-    }),
-    children: $('canvas', {
-      ref: cvsRef,
-      className: css({
-        background: 'green',
-      }),
-    }),
   })
 }
---
+++ packages/browser/src/app/Graph.ts
@@ -0,0 +1,144 @@
+import {css} from '@emotion/css'
+import {createElement as $, FC, useEffect, useRef} from 'react'
+import {theme} from '../theme'
+import {useMedia} from './Media/useMedia'
+/**
+ *
+ */
+export const Graph: FC<{
+  title?: string
+  yLabel?: string
+  xLabel?: string
+  dots?: [number, number][]
+  line?: number[]
+  height?: number
+}> = ({title, yLabel, xLabel, dots, line, height: _height = theme.fib[12]}) => {
+  const media = useMedia()
+  const bgRef = useRef<HTMLCanvasElement>()
+  const cvsRef = useRef<HTMLCanvasElement>()
+  useEffect(() => {
+    if (!cvsRef.current || !bgRef.current) return
+    const cvs = cvsRef.current
+    const height = (cvs.height = _height)
+    const width = (cvs.width = bgRef.current.clientWidth)
+    const ctx = cvs.getContext('2d')!
+    const axis = 55
+    const yMaxDots = dots ? Math.max(...dots.map((i) => i[1])) : 0
+    const yMaxLine = line ? Math.max(...line) : 0
+    const yMax = Math.max(yMaxDots, yMaxLine, 5)
+    const yCount = yMax + Math.round(yMax / 5)
+    const yLength = height - axis
+    const yIncrement = yLength / yCount
+    const ySmall = yIncrement <= 20
+    const xMaxDots = dots ? Math.max(...dots.map((i) => i[0])) : 0
+    const xMaxLine = line ? line.length : 0
+    const xCount = Math.max(xMaxDots, xMaxLine, 5) + 1
+    const xLength = width - axis
+    const xIncrement = xLength / xCount
+    const xSmall = xIncrement <= 20
+    ctx.lineWidth = 2
+    ctx.font = '14px IBM Plex Mono, monospace'
+    {
+      // y axis
+      const x = axis
+      ctx.beginPath()
+      ctx.strokeStyle = 'rgba(0, 0, 0, 1)'
+      ctx.moveTo(x, 0)
+      ctx.lineTo(x, height - axis)
+      ctx.textAlign = 'right'
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)'
+      for (let i = 0; i < yCount; i++) {
+        const y = height - axis - Math.round(i * yIncrement)
+        const bold = ySmall && i % 2 === 0
+        ctx.moveTo(x, y)
+        ctx.lineTo(bold ? x - 10 : x - 5, y)
+        if (!ySmall || i % 2 === 0) ctx.fillText(i.toString(), x - 10, y + 5)
+      }
+      ctx.stroke()
+      ctx.beginPath()
+      ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)'
+      for (let i = 0; i < yCount; i += 2) {
+        const y = height - axis - Math.round(i * yIncrement)
+        ctx.moveTo(x, y)
+        ctx.lineTo(width, y)
+      }
+      ctx.setLineDash([5])
+      ctx.stroke()
+      ctx.setLineDash([])
+      if (yLabel) {
+        ctx.beginPath()
+        ctx.save()
+        ctx.translate(20, yLength / 2)
+        ctx.rotate(-Math.PI / 2)
+        ctx.textAlign = 'center'
+        ctx.fillText(yLabel, 0, 0)
+        ctx.restore()
+      }
+    }
+    {
+      // x axis
+      const y = height - axis
+      ctx.beginPath()
+      ctx.strokeStyle = 'rgba(0, 0, 0, 1)'
+      ctx.moveTo(axis, y)
+      ctx.lineTo(width, y)
+      ctx.textAlign = 'center'
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)'
+      for (let i = 0; i < xCount; i++) {
+        const x = axis + Math.round(i * xIncrement)
+        const bold = xSmall && i % 2 === 0
+        ctx.moveTo(x, y)
+        ctx.lineTo(x, bold ? y + 10 : y + 5)
+        if (!xSmall || i % 2 === 0) ctx.fillText(i.toString(), x, y + 20)
+      }
+      ctx.stroke()
+      if (xLabel) {
+        ctx.beginPath()
+        ctx.textAlign = 'center'
+        ctx.fillText(xLabel, axis + xLength / 2, height - 10)
+      }
+    }
+    if (dots) {
+      ctx.beginPath()
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)'
+      for (let i = 0; i < dots.length; i++) {
+        const x = axis + Math.round(dots[i][0] * xIncrement)
+        const y = height - axis - Math.round(dots[i][1] * yIncrement)
+        ctx.moveTo(x, y)
+        ctx.arc(x, y, 3, 0, Math.PI * 2)
+      }
+      ctx.fill()
+    }
+    if (line) {
+      ctx.beginPath()
+      ctx.textAlign = 'center'
+      ctx.fillStyle = 'rgba(255, 0, 255, 1)'
+      for (let i = 0; i < line.length; i++) {
+        const x = axis + Math.round(i * xIncrement)
+        const y = height - axis - Math.round(line[i] * yIncrement)
+        if (i === 0) ctx.moveTo(x, y)
+        else ctx.lineTo(x, y)
+      }
+      ctx.stroke()
+    }
+    if (title) {
+      ctx.beginPath()
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)'
+      ctx.fillText(title, width / 2, 20)
+    }
+  }, [media.width])
+  return $('div', {
+    ref: bgRef,
+    className: css({
+      display: 'flex',
+      background: theme.bg.string(),
+      border: theme.border(),
+    }),
+    children: $('canvas', {
+      ref: cvsRef,
+      className: css({
+        background: theme.bg.string(),
+      }),
+    }),
+  })
+}
